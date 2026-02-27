9e12b75a770231a9c8486917c4fc924644824efc
navjotmaan <navjotmaan137@gmail.com>
2026-02-27 08:34:06 +0000

    add proxy for server routes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/notes": "http://localhost:3000",
+      "/save": "http://localhost:3000",
+    }
+  }
 })
