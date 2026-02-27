96312b49428f94bff8739f1e06e7dceee86a5031
navjotmaan <navjotmaan137@gmail.com>
2026-02-27 08:38:50 +0000

    write async function to fetch all notes from database

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import AudioRecorder from './components/AudioRecorder'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const getNotes = async () => {
+      const { data } = await axios.get('/notes');
+      setNotes(data);
+      console.log(data);
+    }
+
+    getNotes();
+  }, []);
 
   return (
-      <AudioRecorder />
+    <>
+    <h1 className='font-semibold'>My Life's Diary</h1>
+    <p>We write to taste life twice.</p>
+    <Link to='new' className='border p-1'>New</Link>
+
+    <main>
+      {notes.map(note => (
+        <div key={note.id}>
+          <p>{note.content}</p>
+        </div>
+      ))}
+      
+    </main>
+    </>
   )
 }
 
