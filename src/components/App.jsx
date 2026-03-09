1055e7a3b86f841ce162a8889cd726db0cca9017
navjotmaan <navjotmaan137@gmail.com>
2026-03-09 11:12:32 +0000

    add logic to open and close popup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import QuoteSection from './quotes/Sidebar';
+import Popup from './quotes/Popup';
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
 
   useEffect(() => {
     const getNotes = async () => {
@@ -25,29 +30,50 @@ function App() {
 
   return (
     <div className='flex h-full px-10'>
-      <div className='basis-[70vw] min-h-[100vh]'>
+      <div className='basis-[80vw] min-h-[100vh]'>
         <header className='p-5 text-left my-5'>
             <h1 className='font-bold text-2xl text-[#D9564A]'>My Media</h1>
-            <p className='text-lg mb-5'>We write to taste life twice.</p>
-          <Link to='new' className='rounded-lg py-1 px-3 bg-[#3B9CD9] text-white'>Create</Link>
+            <p className='text-lg mb-5 font-display'>We write to taste life twice.</p>
+
+            <div className='relative inline-block'>
+              <button onClick={toggleDropdown} className='rounded-lg py-1 px-3 bg-[#3B9CD9] text-white cursor-pointer'>
+                Create {isOpen ? '▲' : '▼'}
+              </button>
+
+              {isOpen && (
+                <div onClick={() => setIsOpen(false)} className='absolute mt-1 rounded-lg px-2 flex flex-col items-center justify-center bg-[#f2e9e4] border'>
+                  <Link to='new' className='rounded-lg py-1 px-3'>Journal</Link>
+                  <button onClick={() => setShowPopup(true)} className="rounded-xl p-1 px-2 cursor-pointer">Note</button>
+                </div>
+              )}
+            </div>
+
         </header>
 
         <main className='flex flex-wrap gap-10 p-5'>
-          {notes.map(note => (
-            <Link to={`/${note.id}`} key={note.id} className='border-4 w-[300px] border-[#F2884B] rounded-lg p-5'>
-              <p>{getDate(note.updated_at)}</p>
-              <p>{note.title}</p>
-            </Link>
-          ))}
+          {notes.length !== 0 ? (
+            notes.map(note => (
+              <Link to={`/${note.id}`} key={note.id} className='border-4 w-[300px] h-[100px] border-[#F2884B] rounded-lg p-5'>
+                <p>{getDate(note.updated_at)}</p>
+                <p>{note.title}</p>
+              </Link>
+            ))
+          ) : 
+            <p className='m-auto mt-10 font-handwriting'>Your journals will appear here.</p>
+          }
           
         </main>
       </div>
 
-      <aside>
+      <aside className='border-2 border-[#F2884B] rounded-xl basis-[40vw] h-[100vh] m-10 overflow-auto custom-scrollbar'>
         <QuoteSection />
       </aside>
+
+      {showPopup && (
+          <Popup closePopup={() => setShowPopup(false)}/>
+      )}
     </div>
   )
-}
+};
 
-export default App
+export default App;
