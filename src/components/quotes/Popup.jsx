6c1a028abcff1214c0e1c5cb32eed5a5a6dc02b3
navjotmaan <navjotmaan137@gmail.com>
2026-03-09 11:14:14 +0000

    add post req to delete and put req to update quote

--- src/components/quotes/Popup.jsx
+++ src/components/quotes/Popup.jsx
@@ -1,8 +1,8 @@
 import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 
-const Popup = ({closePopup}) => {
-    const [text, setText] = useState("");
+const Popup = ({ closePopup, quote = "", id = null }) => {
+    const [text, setText] = useState(quote || "");
     const textareaRef = useRef(null);
 
     useEffect(() => {
@@ -13,34 +13,70 @@ const Popup = ({closePopup}) => {
     }, [text]);
 
     useEffect(() => {
-    textareaRef.current?.focus();
+        const ta = textareaRef.current;
+        if (ta) {
+            ta.focus();
+            const length = ta.value.length;
+            ta.setSelectionRange(length, length);
+        }
     }, []);
 
     const saveQuote = async () => {
-        const { data } = await axios.post('/quotes/save', {
-            quote: text,
-        });
-        closePopup();
-        console.log(data);
+        try {
+            if (id) {
+                await axios.put(`/quotes/update/${id}`, {
+                    quote: text,
+                });
+            } else {
+                const { data } = await axios.post('/quotes/save', {
+                    quote: text,
+                });
+            }
+            closePopup();
+        }
+        catch (error) {
+            console.error("Failed to save:", error);
+        }
     };
 
+    const deleteQuote = async () => {
+        try {
+            await axios.post(`/quotes/delete/${id}`);
+            closePopup();
+        } catch (error) {
+            console.error("Failed to delete:", error);
+        }
+    }
+
     return (
-        <div className='bg-[#4a4e69] w-[40%] min-h-50 p-10 rounded-xl fixed top-[50%] left-[50%] translate-[-50%]'>
-            <textarea 
-                ref={textareaRef}
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                placeholder="Start writing…"
-                className='
-                min-h-50 w-full border-none outline-none resize-none
-              placeholder:text-[#a89f96]
-                '
+        <>
+            <div 
+                className="fixed inset-0 bg-black/20 z-40" 
+                onClick={closePopup} 
             />
-            <div>
-                <button className="border p-1" onClick={saveQuote}>Save</button>
-                <button className="border mx-5 p-1" onClick={closePopup}>Cancel</button>
+           
+            <div className='bg-[#D9564A] text-white w-[40%] min-h-50 p-10 rounded-xl fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-50 shadow-2xl'>
+                <textarea 
+                    ref={textareaRef}
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Start writing…"
+                    className='
+                    min-h-50 w-full border-none outline-none resize-none
+                    placeholder:text-[#f2f2f2]
+                    '
+                />
+                <div>
+                    <button className="border p-1 cursor-pointer" onClick={saveQuote}>
+                        {id ? "Update" : "Save"}
+                    </button>
+                    <button className="border mx-5 p-1 cursor-pointer" onClick={closePopup}>Cancel</button>
+                    {id ? 
+                        <button onClick={deleteQuote} className="border p-1 cursor-pointer">Delete</button>
+                    : ''}
+                </div>
             </div>
-        </div>
+        </>
     )
 };
 
