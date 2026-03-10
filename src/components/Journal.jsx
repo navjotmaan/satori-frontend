4d715604b0b2da829303ba4b7935d5aca69270e9
navjotmaan <navjotmaan137@gmail.com>
2026-03-10 02:46:07 +0000

    add useNavigate to navigate to home page on clicking delete

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react"
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Journal = () => {
     const { id } = useParams();
     const [note, setNote] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const getNote = async () => {
@@ -17,6 +18,7 @@ const Journal = () => {
 
     const deleteNote = async () => {
         const { data } = await axios.post(`/delete/${id}`);
+        navigate("/");
     };
 
     return (
