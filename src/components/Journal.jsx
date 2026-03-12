2636d15e4173b34ef91d7bcb7e94dddd16833797
navjotmaan <navjotmaan137@gmail.com>
2026-03-12 14:54:57 +0000

    moved deleteNote function to Dialog and using Dialog component to handle deletion

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react"
 import axios from "axios";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import Dialog from "./Dialog";
 
 const Journal = () => {
     const { id } = useParams();
     const [note, setNote] = useState([]);
-    const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         const getNote = async () => {
@@ -16,18 +17,17 @@ const Journal = () => {
         getNote();
     }, []);
 
-    const deleteNote = async () => {
-        const { data } = await axios.post(`/delete/${id}`);
-        navigate("/");
-    };
-
     return (
         <div className="px-6 py-14">
-            <button onClick={deleteNote} className="border p-1">Delete</button>
+            <button onClick={() => setIsOpen(true)} className="border p-1">Delete</button>
             <main className="w-full max-w-2xl text-left m-auto leading-[1.9] tracking-[0.01em] text-[18px]">
                 <h2 className="text-xl font-bold mb-10">{note.title}</h2>
                 <p className="whitespace-pre-wrap">{note.content}</p>
             </main>
+
+            {isOpen && (
+                <Dialog open={isOpen} onClose={() => setIsOpen(false)} note={true} />
+            )}
         </div>
     )
 };
