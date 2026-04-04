623a4c864df12a9751f99e38ac5185bbad5a4ec8
navjotmaan <navjotmaan137@gmail.com>
2026-04-04 09:37:28 +0000

    add login/signup path and wrap the RouterProvider in AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './components/App.jsx'
+import { AuthProvider } from './api/AuthContext.jsx'
 import Journal from './components/Journal.jsx'
 import Error from './components/ErrorElement.jsx'
 import AudioRecorder from './components/speech-to-text/AudioRecorder.jsx'
+import LoginForm from './forms/login.jsx';
+import SignupForm from './forms/signup.jsx';
+import Dashboard from './components/Dashboard.jsx';
 
 const router = createBrowserRouter([
   {
     errorElement: <Error />,
     children: [
-      { path: '/', element: <App /> },
+      { path: '/', element: <Dashboard /> },
       { path: 'new', element: <AudioRecorder /> },
       { path: ':id', element: <Journal /> },
+      { path: 'login', element: <LoginForm /> },
+      { path: 'signup', element: <SignupForm /> },
     ],
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
