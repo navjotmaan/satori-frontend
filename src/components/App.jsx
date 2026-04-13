44bf95704761135b3fe08bf19e3614e1082434f4
navjotmaan <navjotmaan137@gmail.com>
2026-04-13 17:12:57 +0000

    remove aside and popup element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,104 +2,56 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from "../api/axiosInstance";
 import { useAuth } from '../api/AuthContext';
-import QuoteSection from './quotes/Sidebar';
-import Popup from './quotes/Popup';
-import menu from '../assets/menu.png';
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [quotes, setQuotes] = useState([]);
-  const [showPopup, setShowPopup] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-  const [toggle, setToggle] = useState(false);
+  const [userName, setUserName] = useState('');
 
   const { logout } = useAuth();
-
-  const toggleDropdown = () => setIsOpen(!isOpen);
-
-  const fetchQuotes = async () => {
-    try {
-      const res = await api.get('/quotes');
-      setQuotes([...res.data]);
-    } catch (error) {
-      console.error("Failed to fetch quotes:", error);
-    }
-  };
 
   useEffect(() => {
     const getNotes = async () => {
       const { data } = await api.get('/notes');
+      const user = data[0].name;
+      setUserName(user.slice(0, user.indexOf(" ")));
       setNotes(data);
     };
 
     getNotes();
-    fetchQuotes();
   }, []);
 
+  function getDate(date) {
+    const dateObj = new Date(date);
+    return dateObj.toLocaleDateString('en-US', { 
+        month: 'short', 
+        day: 'numeric' 
+    });
+  }
+
   return (
-    <div className='md:flex relative h-full mx-5'>
-      <div className='md:basis-[80vw] w-full min-h-[100vh]'>
-        <header className='p-5 text-left my-5'>
-            <div className='flex justify-between items-center'>
-              <div>
-                <h1 className='font-bold text-2xl text-[#D9564A]'>My Media</h1>
-                <p className='text-lg mb-5 font-display'>We write to taste life twice.</p>
-              </div>
+    <div className='w-full min-h-[100vh] p-10'>
+      <header className='text-left pb-10'>
+        <h1 className='font-pro text-5xl text-[#2B2D42]'>{userName}'s Media</h1>
+        <p className='text-3xl text-[#6B705C] font-display'>We write to taste life twice.</p>
+      </header>
 
-              <button className='block fixed right-5 top-5 cursor-pointer md:hidden text-xl' onClick={() => setToggle(!toggle)}><img src={menu} alt="Menu" className='w-6'></img></button>
-            </div>
-
-            <div className='relative inline-block'>
-              <button onClick={toggleDropdown} className='rounded-lg py-1 px-3 bg-[#3B9CD9] text-white cursor-pointer'>
-                Create {isOpen ? '▲' : '▼'}
-              </button>
-
-              {isOpen && (
-                <div onClick={() => setIsOpen(false)} className='absolute mt-1 rounded-lg px-2 flex flex-col items-center justify-center bg-[#f2e9e4] border'>
-                  <Link to='new' className='rounded-lg py-1 px-3'>Journal</Link>
-                  <button onClick={() => setShowPopup(true)} className="rounded-xl p-1 px-2 cursor-pointer">Note</button>
-                </div>
-              )}
-            </div>
-
-        </header>
-
-        <main className='flex flex-wrap justify-center md:justify-start gap-10 p-5'>
-          {notes.length !== 0 ? (
-            notes.map(note => (
-              <Link to={`/${note.id}`} key={note.id} className='border-2 w-full md:w-[340px] h-[100px] border-[#F2884B] flex flex-col justify-center rounded-lg p-2 px-3'>
-                <p className='text-xl font-semibold'>{note.title}</p>
-                <p>{note.content.substring(0, 50)}...</p>
-              </Link>
-            ))
-          ) : 
-            <p className='m-auto mt-10 font-handwriting'>Your journals will appear here.</p>
-          }
-          
-        </main>
-      </div>
-
-      {toggle || window.innerWidth >= 768 ? (
-
-        <aside className='border-2 border-[#F2884B] rounded-xl fixed inset-0 z-50 bg-[#f2e9e4] h-[100vh] m-5 mr-0 overflow-auto custom-scrollbar md:z-0 md:sticky md:h-[90vh] md:m-10 md:block md:w-[40vw]'>
-        {/* <button className='rounded-lg py-1 px-3 bg-red-500 text-white cursor-pointer' onClick={logout}>Logout
-          </ button>  */}
-          <button 
-          className="md:hidden absolute cursor-pointer top-1 right-2 text-xl" 
-          onClick={() => setToggle(false)}
-        >
-          ✕
-        </button>
-        <QuoteSection quotes={quotes} onRefresh={fetchQuotes} />
-      </aside>
-      ) : (
-        <></>
-      )}
-
-      {showPopup && (
-        <Popup closePopup={() => setShowPopup(false)} onRefresh={fetchQuotes}/>
-      )}
-    </div>
+      <main className='flex flex-wrap justify-center md:justify-start gap-10'>
+        {notes.length !== 0 ? (
+          notes.map(note => (
+            <Link to={`/${note.id}`} key={note.id} className='w-full md:w-[45%] h-[] border border-[#8C5A3C] shadow-[0_6px_4px_rgba(0,0,0,0.2)] flex flex-col rounded-[30px] p-6'>
+              <span className='flex pb-3 justify-between'>
+                <p className='text-xl text-[#B5651D] font-semibold'>{note.title}</p>
+                <p className='flex items-center bg-[#6B705C] px-5 h-5 text-white text-sm rounded-[30px]'>{getDate(note.created_at)}</p>
+              </span>
+              <p className='text-start'>{note.content.substring(0, 100)}...</p>
+            </Link>
+          ))
+        ) : 
+          <p className='m-auto mt-10 font-handwriting'>Your journals will appear here.</p>
+        }
+        
+      </main>
+  </div>
   )
 };
 
