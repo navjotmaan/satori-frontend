cbda411781802708a61befc7716a485668965249
navjotmaan <navjotmaan137@gmail.com>
2026-04-16 12:57:56 +0000

    remove date and increase title size

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -17,23 +17,12 @@ const Journal = () => {
         getNote();
     }, []);
 
-    function getDate(date) {
-        const dateObj = new Date(date);
-        return dateObj.toLocaleDateString('en-US', { 
-            month: 'short', 
-            day: 'numeric' 
-        });
-    }
-
     return (
         <div className="px-6 py-14">
             <button onClick={() => setIsOpen(true)} className="border fixed right-10 rounded px-2 cursor-pointer top-5 p-1">Delete</button>
             <main className="w-full max-w-2xl text-left m-auto leading-[1.9] tracking-[0.01em] text-[18px]">
                 
-                <span>
-                    <h2 className="text-2xl font-bold">{note.title}</h2>
-                    <p>{getDate(note.created_at)}</p>
-                </span>
+                <h2 className="text-3xl font-bold">{note.title}</h2>
                 <p className="whitespace-pre-wrap mt-10">{note.content}</p>
             </main>
 
