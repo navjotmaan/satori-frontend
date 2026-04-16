726f3763fd4d6e4b3a8b81c1a32e4e884fb8ed10
navjotmaan <navjotmaan137@gmail.com>
2026-04-16 13:00:42 +0000

    add logic to parse user name and email from jwt

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 import Sidebar from "./Sidebar";
 import api from "../api/axiosInstance";
+import { useAuth } from "../api/AuthContext";
 
 const Layout = () => {
     const [userName, setUserName] = useState('');
+    const [email, setEmail] = useState('');
     const [quotes, setQuotes] = useState([]);
+
+    const { accessToken } = useAuth();
 
     const fetchQuotes = async () => {
         try {
@@ -16,15 +20,33 @@ const Layout = () => {
         }
     };
 
+    const parseJwt = (token) => {
+        try {
+            const base64Url = token.split('.')[1]; // Get the payload part
+            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+            return JSON.parse(window.atob(base64));
+        } catch (e) {
+            return null;
+        }
+    };
+
+    useEffect(() => {
+        const userData = parseJwt(accessToken);
+        setEmail(userData?.email || '');
+        setUserName(userData?.name || '');
+    }, [accessToken]);
+
+    const firstName = userName.includes(" ") ? userName.slice(0, userName.indexOf(" ")) : userName;
+
     return (
         <div className="md:flex">
             <Sidebar fetchQuotes={fetchQuotes} />
             <main className="flex-1 ">
                 <header className='text-left p-10'>
-                    <h1 className='font-pro text-5xl text-[#2B2D42]'>{userName}'s Media</h1>
+                    <h1 className='font-pro text-5xl text-[#2B2D42]'>{firstName}'s Media</h1>
                     <p className='text-3xl text-[#6B705C] font-display'>We write to taste life twice.</p>
                 </header>
-                <Outlet context={{ setUserName, quotes, fetchQuotes }} />
+                <Outlet context={{ userName, email, quotes, fetchQuotes }} />
             </main>
         </div>
     );
