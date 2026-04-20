15a157d547ea4f08e3ebab4a5871ad6f72ade820
navjotmaan <navjotmaan137@gmail.com>
2026-04-20 10:57:44 +0000

    add edit option on journal page

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,17 +1,24 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, createContext, useContext } from "react"
 import api from "../api/axiosInstance";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Dialog from "./Dialog";
+import { useJournal } from "./JournalContext";
 
 const Journal = () => {
     const { id } = useParams();
     const [note, setNote] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+    const { setActiveNote } = useJournal();      
 
     useEffect(() => {
         const getNote = async () => {
-        const { data } = await api.get(`/notes/note/${id}`);
-        setNote(data);
+            const { data } = await api.get(`/notes/note/${id}`);
+            setNote(data);
+            setActiveNote({id: id, title: data.title, content: data.content});
         };
 
         getNote();
@@ -19,9 +26,10 @@ const Journal = () => {
 
     return (
         <div className="px-6 py-14">
-            <button onClick={() => setIsOpen(true)} className="border fixed right-10 rounded px-2 cursor-pointer top-5 p-1">Delete</button>
+
+            <svg onClick={toggleDropdown} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="absolute right-10 top-5 lucide lucide-ellipsis-vertical-icon lucide-ellipsis-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg>
+
             <main className="w-full max-w-2xl text-left m-auto leading-[1.9] tracking-[0.01em] text-[18px]">
-                
                 <h2 className="text-3xl font-bold">{note.title}</h2>
                 <p className="whitespace-pre-wrap mt-10">{note.content}</p>
             </main>
@@ -29,7 +37,17 @@ const Journal = () => {
             {isOpen && (
                 <Dialog open={isOpen} onClose={() => setIsOpen(false)} note={true} />
             )}
+
+            {dropdownOpen && (
+                <div onClick={() => setDropdownOpen(false)} className='absolute top-8 right-15 rounded-lg px-2 flex flex-col items-center justify-center w-20 bg-[#FFF8F0] border border-[#4B2E2B] text-[#4B2E2B]'>
+                    <Link to="/new">
+                        <button className='rounded-lg py-1 px-3'>Edit</button>
+                    </Link>
+                    <button onClick={() => setIsOpen(true)} className="rounded-xl p-1 px-2 cursor-pointer">Delete</button>
+                </div>
+            )}
         </div>
+
     )
 };
 
