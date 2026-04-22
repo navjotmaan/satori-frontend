7f7b77d6d9d74d03d6d1f83b1a861a2566135da5
navjotmaan <navjotmaan137@gmail.com>
2026-04-22 15:38:15 +0000

    add cursor-pointer to edit button

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -27,7 +27,7 @@ const Journal = () => {
     return (
         <div className="px-6 py-14">
 
-            <svg onClick={toggleDropdown} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="absolute right-10 top-5 lucide lucide-ellipsis-vertical-icon lucide-ellipsis-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg>
+            <svg onClick={toggleDropdown} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="absolute cursor-pointer right-10 top-5 lucide lucide-ellipsis-vertical-icon lucide-ellipsis-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg>
 
             <main className="w-full max-w-2xl text-left m-auto leading-[1.9] tracking-[0.01em] text-[18px]">
                 <h2 className="text-3xl font-bold">{note.title}</h2>
@@ -41,7 +41,7 @@ const Journal = () => {
             {dropdownOpen && (
                 <div onClick={() => setDropdownOpen(false)} className='absolute top-8 right-15 rounded-lg px-2 flex flex-col items-center justify-center w-20 bg-[#FFF8F0] border border-[#4B2E2B] text-[#4B2E2B]'>
                     <Link to="/new">
-                        <button className='rounded-lg py-1 px-3'>Edit</button>
+                        <button className='rounded-lg py-1 px-3 cursor-pointer'>Edit</button>
                     </Link>
                     <button onClick={() => setIsOpen(true)} className="rounded-xl p-1 px-2 cursor-pointer">Delete</button>
                 </div>
